19456025ef23f57c17ee73b92d7eae6c93332609
Jonny Graybill <jon.a.graybill@gmail.com>
2019-05-11 11:31:36 -0700

    more confusion

--- server.js
+++ server.js
@@ -1,51 +1,40 @@
-'use strict';
+'use strict'
 
-// PROVIDE ACCESS TO ENVIRONMENTAL VARIABLES IN .env
+// Load environment variables
 require('dotenv').config();
 
-// LOAD APP DEPENDENDIES
+//Load express to do the heavey lifting -- Application dependencies
 const express = require('express');
 const superagent = require('superagent');
-const cors = require('cors');
-const pg = require('pg');
+const cors = require('cors'); //Cross Origin Resource Sharing
+const pg = require('pg'); //postgress
 
-// APP SETUP
+//Application setup
 const app = express();
-app.use(cors());
+app.use(cors()); //tell express to use cors
 const PORT = process.env.PORT;
 
-//Connecting to the database
-const client = new pg.Client(process.env.DATABASE_URL);
+//connect to database
+const client = new pg.Client(process.env.DATABASE_URL); // part of posgres library
 client.connect();
 client.on('error', err => console.log(err));
 
-//API routes
-
-app.get('/location', searchToLatLong);
+app.get('/location', searchToLatLong)
 app.get('/weather', getWeather);
-app.get('/events', getEvents);
+app.get('/events', getEvent);
 app.get('/movies', getMovies);
-app.get('/yelps', getYelps);
+app.get('/yelp', getYelp);
+app.get('/trails', getTrails);
 
-// TURN THE SERVER ON
-app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+//server listening for requests
+app.listen(PORT, ()=> console.log(`city explorer back end Listening on PORT ${PORT}`));
 
-// ERROR HANDLER
-function handleError(err, response) {
-  console.error(err);
-  if (response) response.status(500).send('Sorry, something is not right');
-}
-
-// HELPER FUNCTIONS
-
-//Function to get location data
-
-function getDataFromDB(sqlInfo) {
-  // Create a SQL Statement
+function getDataFromDB(sqlInfo){
+  // create sql statement
   let condition = '';
   let values = [];
 
-  if (sqlInfo.searchQuery) {
+  if (sqlInfo.searchQuery){
     condition = 'search_query';
     values = [sqlInfo.searchQuery];
   } else {
@@ -53,62 +42,58 @@ function getDataFromDB(sqlInfo) {
     values = [sqlInfo.id];
   }
 
-  let sql = `SELECT * FROM ${sqlInfo.endpoint}s WHERE ${condition}=$1;`;
+  let sql = `SELECT * FROM ${sqlInfo.endpoint}s WHERE ${condition}=$1`;
 
-  // Get the Data and Return
-  try { return client.query(sql, values); }
-  catch (error) { handleError(error); }
+  // Get the data and return it
+  try {return client.query(sql, values);}
+  catch(error){ handleError(error);}
 }
 
-function saveDataToDB(sqlInfo) {
-  // Create the parameter placeholders
+function saveDataToDB(sqlInfo){
+  // param placeholder
   let params = [];
 
-  for (let i = 1; i <= sqlInfo.values.length; i++) {
-    params.push(`$${i}`);
+  for (let i=1; i<= sqlInfo.values.length;i++){
+    params.push(`$${i}`)
   }
 
   let sqlParams = params.join();
 
   let sql = '';
-  if (sqlInfo.searchQuery) {
-    // location
-    sql = `INSERT INTO ${sqlInfo.endpoint}s (${sqlInfo.columns})
-    VALUES (${sqlParams}) RETURNING ID;`;
+  if (sqlInfo.searchQuery){
+    // loc
+    sql = `INSERT INTO ${sqlInfo.endpoint}s (${sqlInfo.columns}) VALUES (${sqlParams}) RETURNING ID;`
   } else {
-    //all other endpoints
-    sql = `INSERT INTO ${sqlInfo.endpoint}s (${sqlInfo.columns}) VALUES (${sqlParams});`;
+    // all other endpoints since loc is a little special
+    sql = `INSERT INTO ${sqlInfo.endpoint}s (${sqlInfo.columns}) VALUES (${sqlParams});`
   }
 
-  // save the data
-  try { return client.query(sql, sqlInfo.values); }
-  catch (err) { handleError(err); }
+  // save all this hardly won data nom nom nom
+
+  try {return client.query(sql, sqlInfo.values);}
+  catch(err){handleError(err);}
 }
 
-//Cache Invalidation:
-
-//bunch of steps
-
-//Check to see if the data is still valid
 function checkTimeouts(sqlInfo, sqlData) {
+
   const timeouts = {
-    weather: 15 * 1000, //15 seconds
-    yelp: 24 * 1000 * 60 * 60, //24 hours
-    movie: 30 * 1000 * 60 * 60 * 24, //30 days
-    event: 6 * 1000 * 60 * 60, //6 hours
-    trail: 7 * 1000 * 60 * 60 * 24 //7 days
+    weather: 15 * 1000, // 15-seconds
+    yelp: 24 * 1000 * 60 * 60, // 24-Hours
+    movie: 30 * 1000 * 60 * 60 * 24, // 30-Days
+    event: 6 * 1000 * 60 * 60, // 6-Hours
+    trail: 7 * 1000 * 60 * 60 * 24 // 7-Days
   };
 
-  //if there is data, find out how old it is
+  // if there is data, find out how old it is.
   if (sqlData.rowCount > 0) {
     let ageOfResults = (Date.now() - sqlData.rows[0].created_at);
 
-    //for debugging only
+    // For debugging only
     console.log(sqlInfo.endpoint, ' AGE:', ageOfResults);
     console.log(sqlInfo.endpoint, ' Timeout:', timeouts[sqlInfo.endpoint]);
 
-    //Compare the age of the results with the timeout value
-    //Delete the data if it is old
+    // Compare the age of the results with the timeout value
+    // Delete the data if it is old
     if (ageOfResults > timeouts[sqlInfo.endpoint]) {
       let sql = `DELETE FROM ${sqlInfo.endpoint}s WHERE location_id=$1;`;
       let values = [sqlInfo.id];
@@ -119,29 +104,24 @@ function checkTimeouts(sqlInfo, sqlData) {
   }
 }
 
-function searchToLatLong(request, response) {
+function searchToLatLong(request, response){
   let sqlInfo = {
     searchQuery: request.query.data,
     endpoint: 'location'
   };
-
   getDataFromDB(sqlInfo)
     .then(result => {
-      if (result.rowCount > 0) {
+      if (result.rowCount >0){
         response.send(result.rows[0]);
       } else {
         const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
-
         superagent.get(url)
           .then(result => {
-            if (!result.body.results.length) { throw 'NO DATA'; }
+            if (!result.body.results.length) {throw 'NO DATA';}
             else {
-              let location = new Location(sqlInfo.searchQuery,
-                result.body.results[0]);
-
+              let location = new Location(sqlInfo.searchQuery, result.body.results[0]);
               sqlInfo.columns = Object.keys(location).join();
               sqlInfo.values = Object.values(location);
-
               saveDataToDB(sqlInfo)
                 .then(data => {
                   location.id = data.rows[0].id;
@@ -149,9 +129,9 @@ function searchToLatLong(request, response) {
                 });
             }
           })
-          .catch(error => handleError(error, response));
+          .catch(err => handleError(err, response));
       }
-    });
+    })
 }
 
 function getWeather(request, response) {
@@ -161,58 +141,54 @@ function getWeather(request, response) {
   };
 
   getDataFromDB(sqlInfo)
-    .then(data => checkTimeouts(sqlInfo, data))
+    .then(data => checkTimeouts(sqlInfo,data))
     .then(result => {
-      if (result) { response.send(result.rows); }
+      if (result) {response.send(result.rows);}
       else {
         const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
 
         return superagent.get(url)
           .then(weatherResults => {
-            // console.log('Weather from API');
+            // console.log('line 101','weather from API');
             if (!weatherResults.body.daily.data.length) { throw 'NO DATA'; }
             else {
-              const weatherSummaries = weatherResults.body.daily.data.map(day => {
+              const weatherSummaries = weatherResults.body.daily.data.map( day => {
                 let summary = new Weather(day);
                 summary.location_id = sqlInfo.id;
 
                 sqlInfo.columns = Object.keys(summary).join();
                 sqlInfo.values = Object.values(summary);
 
-                saveDataToDB(sqlInfo);
+                saveDataToDB(sqlInfo)
                 return summary;
               });
               response.send(weatherSummaries);
             }
           })
-          .catch(error => handleError(error, response));
+          .catch(err => handleError(err, response));
       }
     });
 }
 
-
-function getEvents(request, response) {
-  // console.log(request.query.data.id, 'ffffffuuuuuuuu');
+function getEvent(request, response) {
   let sqlInfo = {
     id: request.query.data.id,
     endpoint: 'event'
   };
 
-
   getDataFromDB(sqlInfo)
     .then(data => checkTimeouts(sqlInfo, data))
     .then(result => {
-      if (result) { response.send(result.rows); }
+      if(result){response.send(result.rows);}
       else {
-        const url = `https://www.eventbriteapi.com/v3/events/search?token=${process.env.EVENTBRITE_API_KEY}&location.address=${request.query.data.formatted_query}`;
+        const url = `https://www.eventbriteapi.com/v3/events/search/?token=${process.env.EVENTBRITE_API_KEY}&location.latitude=${request.query.data.latitude}&location.longitude=${request.query.data.longitude}`;
 
         return superagent.get(url)
           .then(eventResults => {
             if (!eventResults.body.events.length) { throw 'NO DATA'; }
             else {
-              // console.log(eventResults);
-              const eventSummaries = eventResults.body.events.map(event => {
-                let summary = new Event(event);
+              const eventSummaries = eventResults.body.events.map( events => {
+                let summary = new Event(events);
                 summary.location_id = sqlInfo.id;
 
                 sqlInfo.columns = Object.keys(summary).join();
@@ -224,7 +200,7 @@ function getEvents(request, response) {
               response.send(eventSummaries);
             }
           })
-          .catch(error => handleError(error, response));
+          .catch(err => handleError(err, response));
       }
     });
 }
@@ -234,22 +210,22 @@ function getMovies(request, response) {
     id: request.query.data.id,
     endpoint: 'movie'
   };
-  
+
   getDataFromDB(sqlInfo)
     .then(data => checkTimeouts(sqlInfo,data))
     .then(result => {
-      // console.log('Hi there')
       if (result) {response.send(result.rows);}
       else {
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${request.query.data.formatted_query.split(',')[0]}`;
-        // console.log(request.query.data.formatted_query.split(',')[0]);
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${request.query.data.formatted_address.split(',')[0]}&page=1&include_adult=false`;
+        console.log(request.query.data.formatted_address);
+
         return superagent.get(url)
           .then(movieResults => {
+
             if (!movieResults.body.results.length) { throw 'NO DATA'; }
             else {
               const movieSummaries = movieResults.body.results.map( movie => {
                 let summary = new Movie(movie);
-
                 summary.location_id = sqlInfo.id;
                 sqlInfo.columns = Object.keys(summary).join();
                 sqlInfo.values = Object.values(summary);
@@ -265,28 +241,27 @@ function getMovies(request, response) {
     });
 }
 
-function getYelps(request, response) {
-
+function getYelp (request, response) {
   let sqlInfo = {
     id: request.query.data.id,
     endpoint: 'yelp'
   };
-
   getDataFromDB(sqlInfo)
-    .then(data => checkTimeouts(sqlInfo, data))
+    .then(data => checkTimeouts(sqlInfo,data))
     .then(result => {
-      if (result) { response.send(result.rows); }
+      if (result) {response.send(result.rows);}
       else {
-      // .set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
         const url = `https://api.yelp.com/v3/businesses/search?latitude=${request.query.data.latitude}&longitude=${request.query.data.longitude}`;
 
         return superagent.get(url)
+          .set({'Authorization': 'Bearer '+ process.env.YELP_API_KEY})
           .then(yelpResults => {
-            console.log('Yelp from API');
-            if(!yelpResults.body.results.length) { throw 'NO DATA';}
+
+            if (!yelpResults.body.businesses.length) { throw 'NO DATA'; }
             else {
-              const yelpSummaries = yelpResults.body.results.map(yelp => {
-                let summary = new Yelp(yelp);
+              const yelpSummaries = yelpResults.body.businesses.map( biz => {
+                let summary = new Yelp(biz);
+
                 summary.location_id = sqlInfo.id;
 
                 sqlInfo.columns = Object.keys(summary).join();
@@ -298,50 +273,48 @@ function getYelps(request, response) {
               response.send(yelpSummaries);
             }
           })
-          .catch(err => handleError(error, response));
+          .catch(err => handleError(err, response));
       }
     });
 }
 
-// function getEvents(request, response) {
-//   let query = request.query.data.id;
-//   let sql = `SELECT * FROM events WHERE location_id=$1;`;
-//   let values = [query];
+function getTrails (request, response){
+  let sqlInfo = {
+    id: request.query.data.id,
+    endpoint: 'trail'
+  };
+  getDataFromDB(sqlInfo)
+    .then(data => checkTimeouts(sqlInfo,data))
+    .then(result => {
+      if(result){response.send(result.rows);}
+      else {
+        const url = `https://www.hikingproject.com/data/get-trails?lat=${request.query.data.latitude}&lon=${request.query.data.longitude}&key=${process.env.TRAIL_API_KEY}`;
 
-//   client.query(sql, values)
-//     .then(result => {
-//       if (result.rowCount > 0) {
-//         response.send(result.rows);
-//       } else {
-//         const url = `https://www.eventbriteapi.com/v3/events/search?token=${process.env.EVENTBRITE_API_KEY}&location.address=${request.query.data.formatted_query}`;
+        return superagent.get(url)
+          .then(trailResults => {
+            if(!trailResults.body.trails.length){throw 'NO DATA';}
+            else {
+              const trailSummaries = trailResults.body.trails.map(trail => {
+                let summary = new Trail(trail);
+                summary.location_id = sqlInfo.id;
+                sqlInfo.columns = Object.keys(summary).join();
+                sqlInfo.values = Object.values(summary);
 
-//         return superagent.get(url)
-//           .then(result => {
-//             if (!result.body.events.length) { throw 'NO DATA'; }
-//             else {
-//               const eventSummaries = result.body.events.map(events => {
-//                 let event = new Event(events);
-//                 event.id = query;
+                saveDataToDB(sqlInfo);
+                return summary;
+              });
+              response.send(trailSummaries);
+            }
+          })
+          .catch(err => handleError(err, response));
+      }
+    });
+}
 
-//                 let newSQL = `INSERT INTO events (link, name, event_date, summary, location_id) VALUES ($1, $2, $3, $4, $5);`;
-//                 let newValues = Object.values(event);
-
-//                 client.query(newSQL, newValues);
-
-//                 return event;
-//               });
-//               response.send(eventSummaries.slice(0, 20));
-//             }
-//           })
-//           .catch(error => handleError(error, response));
-//       }
-//     });
-// }
-
-
+// Constructors
 function Location(query, location) {
   this.search_query = query;
-  this.formatted_query = location.formatted_address;
+  this.formatted_address = location.formatted_address;
   this.latitude = location.geometry.location.lat;
   this.longitude = location.geometry.location.lng;
 }
@@ -352,33 +325,50 @@ function Weather(day) {
   this.created_at = Date.now();
 }
 
-//eventbrite constructor
 function Event(event) {
   this.link = event.url;
   this.name = event.name.text;
-  this.event_date = new Date(event.start.local).toDateString();
   this.summary = event.summary;
+  this.event_date = new Date(event.start.local).toString().slice(0, 15);
   this.created_at = Date.now();
 }
 
-//moviedb constructor
-function Movie(movie){
+function Movie (movie) {
   this.title = movie.original_title;
-  this.overview = movie.overview;
+  this.overview = movie.overview.slice(0,750);
   this.average_votes = movie.vote_average;
   this.total_votes = movie.vote_count;
-  this.image_url = `https://image.tmdb.org/t/p/w185/${movie.poster_path}`;
+  this.image_url = `https://image.tmdb.org/t/p/original${movie.poster_path}` ;
   this.popularity = movie.popularity;
-  this.released_on = new Date(movie.release_date).toDateString();
+  this.released_on = movie.release_date;
   this.created_at = Date.now();
 }
 
-//yelp constructor
-function Yelp(yelp){
+function Yelp (yelp) {
   this.name = yelp.name;
   this.image_url = yelp.image_url;
   this.price = yelp.price;
   this.rating = yelp.rating;
   this.url = yelp.url;
   this.created_at = Date.now();
+}
+
+function Trail (trail){
+  this.name = trail.name;
+  this.location = trail.location;
+  this.length = trail.length;
+  this.stars = trail.stars;
+  this.star_votes = trail.starVotes;
+  this.summary = trail.summary;
+  this.trail_url = trail.url;
+  this.conditions = trail.conditionDetails;
+  this.condition_date = trail.conditionDate.split(' ')[0];
+  this.condition_time = trail.conditionDate.split(' ')[1];
+  this.created_at = Date.now();
+}
+
+//error handler
+function handleError(err, response) {
+  console.log(err);
+  if (response) response.status(500).send('Sorry something went wrong');
 }
